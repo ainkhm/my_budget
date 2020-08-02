6fcc34cbe4f5fb51b3983e3eb21888b1c6c57987
Andrii Khmelovskyi <a.i.khmelovsky@gmail.com>
2020-08-02 20:23:48 +0300

    components initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-
 import "./App.css";
 
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </div>
+  );
 }
 
 export default App;
